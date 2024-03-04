c0459c764ee4e98e0d2b4614d2608502b070f4d8
MattiaBologna <mattiabologna01@gmail.com>
2024-03-04 16:25:56 +0100

    Add: spice

--- js/app.js
+++ js/app.js
@@ -10,14 +10,23 @@ console.log("Traveler's age: " + age + ".  Kilometers: " + kilometers)
 const fullPrice = (kilometers * 0.21); //number
 const roundedFullPrice = Math.round((fullPrice + Number.EPSILON)*100) / 100; //number
 console.log("Full price is: " + roundedFullPrice + "€");
+let trainTicketPrice = document.getElementById('train-ticket-price')
+trainTicketPrice.innerHTML = roundedFullPrice + "€"
 
 // Apply the discount 
 if (age < 18) {
     let discountedPrice = fullPrice - (fullPrice * 0.2); //number
     let roundedDiscountedPrice = Math.round((discountedPrice + Number.EPSILON)*100) / 100; //number
     console.log("The discounted price is: " + roundedDiscountedPrice + "€");
+    let trainTicketPrice = document.getElementById('train-ticket-price')
+    trainTicketPrice.innerHTML = roundedDiscountedPrice + "€";
 } else if (age >= 65) {
     let discountedPrice = fullPrice - (fullPrice * 0.4); //number
     let roundedDiscountedPrice = Math.round((discountedPrice + Number.EPSILON)*100) / 100; //number
     console.log("The discounted price is: " + roundedDiscountedPrice + "€");
+    let trainTicketPrice = document.getElementById('train-ticket-price')
+    trainTicketPrice.innerHTML = roundedDiscountedPrice + "€";
 }
+
+
+// Print the price 
