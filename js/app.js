c555b0695056c96ea02af7660570f7f6e1fa1d96
MattiaBologna <mattiabologna01@gmail.com>
2024-03-04 15:13:59 +0100

    Add: calculate full price

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 // Travelers and travel data 
 
-const age = prompt("What's the traveler's age?")
-const kilometers = prompt("How many kilometers will you travel?")
+const age = parseFloat(prompt("What's the traveler's age?")); //number
+const kilometers = parseFloat(prompt("How many kilometers will you travel?")); //number
 
 console.log("Traveler's age: " + age + ".  Kilometers: " + kilometers)
 
 // Calculate full price 
+
+const fullPrice = (kilometers * 0.21);
+console.log("Full price is: " + fullPrice)
 
 // Apply the discount 
 
