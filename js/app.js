5b757846941f64b65b8fc8841a3d89698da7393f
MattiaBologna <mattiabologna01@gmail.com>
2024-03-04 15:10:24 +0100

    Add: Age and Kilometers variables definition

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Travelers and travel data 
+
+const age = prompt("What's the traveler's age?")
+const kilometers = prompt("How many kilometers will you travel?")
+
+console.log("Traveler's age: " + age + ".  Kilometers: " + kilometers)
+
+// Calculate full price 
+
+// Apply the discount 
+
+// Printing the final price 
