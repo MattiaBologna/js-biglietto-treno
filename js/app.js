5baef0f16c219ed7471d3696f47fbae5f30b8824
MattiaBologna <mattiabologna01@gmail.com>
2024-03-04 15:21:10 +0100

    Add: discounted price

--- js/app.js
+++ js/app.js
@@ -7,9 +7,15 @@ console.log("Traveler's age: " + age + ".  Kilometers: " + kilometers)
 
 // Calculate full price 
 
-const fullPrice = (kilometers * 0.21);
-console.log("Full price is: " + fullPrice)
+const fullPrice = (kilometers * 0.21); //number
+console.log("Full price is: " + fullPrice);
 
 // Apply the discount 
-
+if (age < 18) {
+    let discountedPrice = fullPrice - (fullPrice * 0.2); //number
+    console.log("The discounted price is: " + discountedPrice);
+} else if (age >= 65) {
+    let discountedPrice = fullPrice - (fullPrice * 0.4); //number
+    console.log("The discounted price is: " + discountedPrice);
+}
 // Printing the final price 
