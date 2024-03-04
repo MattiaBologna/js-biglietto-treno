1c616226d3082c1e749dab5ece6bb90f555241aa
MattiaBologna <mattiabologna01@gmail.com>
2024-03-04 16:47:29 +0100

    De-Fix: Math.round() --> toFixed()

--- js/app.js
+++ js/app.js
@@ -1,31 +1,28 @@
 // Travelers and travel data 
 
-const age = parseFloat(prompt("What's the traveler's age?")); //number
-const kilometers = parseFloat(prompt("How many kilometers will you travel?")); //number
+const age = parseInt(prompt("What's the traveler's age?")); //number
+const kilometers = parseInt(prompt("How many kilometers will you travel?")); //number
 
 console.log("Traveler's age: " + age + ".  Kilometers: " + kilometers)
 
 // Calculate full price 
 
 const fullPrice = (kilometers * 0.21); //number
-const roundedFullPrice = Math.round((fullPrice + Number.EPSILON)*100) / 100; //number
-console.log("Full price is: " + roundedFullPrice + "€");
+console.log("Full price is: " + fullPrice.toFixed(2) + "€");
 let trainTicketPrice = document.getElementById('train-ticket-price')
-trainTicketPrice.innerHTML = roundedFullPrice + "€"
+trainTicketPrice.innerHTML = fullPrice.toFixed(2) + "€"
 
 // Apply the discount 
 if (age < 18) {
     let discountedPrice = fullPrice - (fullPrice * 0.2); //number
-    let roundedDiscountedPrice = Math.round((discountedPrice + Number.EPSILON)*100) / 100; //number
-    console.log("The discounted price is: " + roundedDiscountedPrice + "€");
+    console.log("The discounted price is: " + discountedPrice.toFixed(2) + "€");
     let trainTicketPrice = document.getElementById('train-ticket-price')
-    trainTicketPrice.innerHTML = roundedDiscountedPrice + "€";
+    trainTicketPrice.innerHTML = discountedPrice.toFixed(2) + "€";
 } else if (age >= 65) {
     let discountedPrice = fullPrice - (fullPrice * 0.4); //number
-    let roundedDiscountedPrice = Math.round((discountedPrice + Number.EPSILON)*100) / 100; //number
-    console.log("The discounted price is: " + roundedDiscountedPrice + "€");
+    console.log("The discounted price is: " + discountedPrice.toFixed(2) + "€");
     let trainTicketPrice = document.getElementById('train-ticket-price')
-    trainTicketPrice.innerHTML = roundedDiscountedPrice + "€";
+    trainTicketPrice.innerHTML = discountedPrice.toFixed(2) + "€";
 }
 
 
