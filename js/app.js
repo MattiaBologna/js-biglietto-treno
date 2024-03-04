3a1adf0dcf0afbcb3433649cf71ad5d7f838e862
MattiaBologna <mattiabologna01@gmail.com>
2024-03-04 17:50:44 +0100

    Fix: const fullPrice --> let fullPrice

--- js/app.js
+++ js/app.js
@@ -7,23 +7,24 @@ console.log("Traveler's age: " + age + ".  Kilometers: " + kilometers)
 
 // Calculate full price 
 
-const fullPrice = (kilometers * 0.21); //number
-console.log("Full price is: " + fullPrice.toFixed(2) + "€");
+let fullPrice = (kilometers * 0.21); //number
 let trainTicketPrice = document.getElementById('train-ticket-price')
 trainTicketPrice.innerHTML = fullPrice.toFixed(2) + "€"
 
 // Apply the discount 
 if (age < 18) {
-    let discountedPrice = fullPrice - (fullPrice * 0.2); //number
-    console.log("The discounted price is: " + discountedPrice.toFixed(2) + "€");
+    fullPrice = fullPrice - (fullPrice * 0.2); //number
     let trainTicketPrice = document.getElementById('train-ticket-price')
-    trainTicketPrice.innerHTML = discountedPrice.toFixed(2) + "€";
+    trainTicketPrice.innerHTML = fullPrice.toFixed(2) + "€";
 } else if (age >= 65) {
-    let discountedPrice = fullPrice - (fullPrice * 0.4); //number
-    console.log("The discounted price is: " + discountedPrice.toFixed(2) + "€");
+    fullPrice = fullPrice - (fullPrice * 0.4); //number
     let trainTicketPrice = document.getElementById('train-ticket-price')
-    trainTicketPrice.innerHTML = discountedPrice.toFixed(2) + "€";
+    trainTicketPrice.innerHTML = fullPrice.toFixed(2) + "€";
 }
 
 
 // Print the price 
+console.log("Full price is: " + fullPrice.toFixed(2) + "€");
+if (age < 18 || age >= 65) {
+    console.log("The discounted price is: " + fullPrice.toFixed(2) + "€");
+}
